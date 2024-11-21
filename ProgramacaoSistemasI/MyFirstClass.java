addeaf0a662e66cc64371b7fc114f9604c61a9db
Mateus <125222236+Mateus-Allebrand@users.noreply.github.com>
2024-11-20 22:40:10 -0300

    Create MyFirstClass.java

---
+++ ProgramacaoSistemasI/MyFirstClass.java
@@ -0,0 +1,12 @@
+public class MyFirstClass
+{
+
+    public static void main(String... args) {
+        System.out.println("Hello, World!");
+    }
+
+    @Override
+    public String toString() {
+        return "MyFirstClass []";
+    }
+}
